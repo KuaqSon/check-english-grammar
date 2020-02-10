51bb4799c02fa7697f204724b8dda97948bbc3c4
KuaqSon <qson.ng@gmail.com>
2020-02-10 17:32:52 +0700

    Build frontend

---
+++ server/public/precache-manifest.065b3957ea08c0729475d6338ccc45a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2cf031fea7177e344734d776c3775d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34af8d0a55ca340b1de0",
+    "url": "/static/css/main.d77b4802.chunk.css"
+  },
+  {
+    "revision": "c43b833c01ccaeb04ab7",
+    "url": "/static/js/2.12cceb7d.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.12cceb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34af8d0a55ca340b1de0",
+    "url": "/static/js/main.ba6a7ba2.chunk.js"
+  },
+  {
+    "revision": "f45573767e956c370bd9",
+    "url": "/static/js/runtime-main.6f19d6a4.js"
+  }
+]);
